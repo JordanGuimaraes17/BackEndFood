90a365eb092d5fd4b0a3704a1e1b2c8e2cb5444e
Jordan Guimarães <jordanggomes@gmail.com>
2024-01-23 10:09:46 -0300

    middlewares

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 const { Router } = require('express')
 
+const sessionsRouter = require('./sessions.routes')
 const usersRouter = require('./users.routes')
 const categoriesRouter = require('./categorie.routes')
 const dishesRouter = require('./dishes.routes')
+
 const ordersRouter = require('./orders.routes')
 const ratingRouter = require('./rating.routes')
 const ordersDishesRouter = require('./OrdersDishes.routes')
 
 const routes = Router()
+routes.use('/sessions', sessionsRouter)
 routes.use('/rating', ratingRouter)
 routes.use('/users', usersRouter)
 routes.use('/categories', categoriesRouter)

6fa5f93c329bcaf61de43b2367b262785cfe6365
Jordan Guimarães <jordanggomes@gmail.com>
2024-01-26 09:01:59 -0300

    integracao-front-end-e-back-end

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 require('express-async-errors')
 const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
+const cors = require('cors')
+
 const express = require('express')
 const routes = require('./routes')
 migrationsRun()
 
 const app = express()
+app.use(cors())
 app.use(express.json()) // receber informações do body
+
 app.use(routes)
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {

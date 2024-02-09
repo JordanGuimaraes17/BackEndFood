e1b60359fea46693d718b6d91da1cb04a08d388c
Jordan Guimarães <jordanggomes@gmail.com>
2024-02-09 11:04:47 -0300

    integracao-front-end-e-back-end

--- src/controllers/OrdersController.js
+++ src/controllers/OrdersController.js
@@ -61,30 +61,30 @@ class OrdersController {
       const orderDetails = await knex('orders')
         .select(
           'orders.id',
-          'orders.quantity AS order_quantity', // Renomeie para evitar conflito com o campo 'quantity' de dishes
+          'orders.quantity AS order_quantity',
           'orders.dish_price',
-          knex.raw('orders.quantity * orders.dish_price AS total_price'), // Calcule o preço total do prato multiplicando a quantidade pelo preço unitário
+          knex.raw('orders.quantity * orders.dish_price AS total_price'), // total do prato multiplicando a quantidade pelo preço unitário
           'dishes.id AS dish_id',
           'dishes.name AS dish_name',
           'dishes.description AS dish_description',
           'dishes.price AS dish_price',
-          'categories.name AS category_name' // Adicione a coluna da categoria do prato
+          'categories.name AS category_name'
         )
         .leftJoin('dishes', 'orders.dish_id', 'dishes.id')
-        .leftJoin('categories', 'dishes.category_id', 'categories.id') // Faça um join com a tabela de categorias
+        .leftJoin('categories', 'dishes.category_id', 'categories.id')
         .where({ 'orders.user_id': user_id })
 
       if (orderDetails.length === 0) {
         return response.status(404).json({ message: 'Pedido não encontrado' })
       }
 
-      // Calcule o total do pedido levando em consideração a quantidade de cada prato
+      //  total do pedido levando em consideração a quantidade de cada prato
       const totalOrderPrice = orderDetails.reduce(
-        (total, dish) => total + dish.total_price, // Use o total_price diretamente, pois já foi calculado corretamente
+        (total, dish) => total + dish.total_price,
         0
       )
 
-      // Retorne os detalhes do pedido e o total
+      //  detalhes do pedido e o total
       return response.json({
         orderDetails,
         totalOrderPrice
@@ -97,7 +97,7 @@ class OrdersController {
 
   async removeOrder(req, res) {
     try {
-      const { dish_id } = req.body
+      const { dish_id, removeAll } = req.body
       const user_id = req.user.id
 
       if (!dish_id || dish_id.length === 0) {
@@ -113,27 +113,35 @@ class OrdersController {
         throw new AppError('Prato não encontrado no pedido.')
       }
 
-      if (existingOrder.quantity > 1) {
-        // Se a quantidade for maior que 1, apenas diminua a quantidade
-        const newQuantity = existingOrder.quantity - 1
+      if (removeAll) {
+        // Remover todas as unidades do prato do pedido
+        await knex('orders').where({ user_id, dish_id }).del()
+      } else {
+        // Reduzir a quantidade do prato no pedido em 1 unidade
+        let newQuantity = existingOrder.quantity - 1
+        if (newQuantity < 0) {
+          newQuantity = 0 // Não permitir quantidades negativas
+        }
         const newTotalPrice = newQuantity * existingOrder.dish_price
 
-        await knex('orders')
-          .where({ user_id, dish_id })
-          .update({ quantity: newQuantity, total_price: newTotalPrice })
-      } else {
-        // Se a quantidade for 1, remova o prato do pedido
-        await knex('orders').where({ user_id, dish_id }).del()
+        if (newQuantity === 0) {
+          // Se a quantidade for zero, remova o prato do pedido
+          await knex('orders').where({ user_id, dish_id }).del()
+        } else {
+          // Atualize a quantidade e o preço total do prato no pedido
+          await knex('orders')
+            .where({ user_id, dish_id })
+            .update({ quantity: newQuantity, total_price: newTotalPrice })
+        }
       }
 
       return res.json({
-        message: 'Uma unidade do prato removida do pedido com sucesso.'
+        message: `Prato removido do pedido com sucesso.`
       })
     } catch (error) {
       console.error(error)
       return res.status(500).json({
-        error:
-          error.message || 'Erro ao remover uma unidade do prato do pedido.'
+        error: error.message || 'Erro ao remover prato do pedido.'
       })
     }
   }
